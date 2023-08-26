f8d3f667a4271d9b4c56642de46dc0b2aee807f0
venkat9507 <70470427+venkat9507@users.noreply.github.com>
2023-08-26 22:59:41 +0530

    android method channel pushing

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,46 @@
+#import <Flutter/Flutter.h>
+#import "GeneratedPluginRegistrant.h"
+
+@implementation AppDelegate
+- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
+    FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+    
+    FlutterMethodChannel* batteryChannel = [FlutterMethodChannel
+                                            methodChannelWithName:@"com.venkat.battery"
+                                            binaryMessenger:controller.binaryMessenger];
+    
+    [batteryChannel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+        __weak typeof(self) weakSelf = self;
+        [batteryChannel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+          // This method is invoked on the UI thread.
+          if ([@"getBatteryLevel" isEqualToString:call.method]) {
+            int batteryLevel = [weakSelf getBatteryLevel];
+
+            if (batteryLevel == -1) {
+              result([FlutterError errorWithCode:@"UNAVAILABLE"
+                                         message:@"Battery level not available."
+                                         details:nil]);
+            } else {
+              result(@(batteryLevel));
+            }
+          } else {
+            result(FlutterMethodNotImplemented);
+          }
+        }];
+        // This method is invoked on the UI thread.
+        // TODO
+    }];
+    
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+- (int)getBatteryLevel {
+  UIDevice* device = UIDevice.currentDevice;
+  device.batteryMonitoringEnabled = YES;
+  if (device.batteryState == UIDeviceBatteryStateUnknown) {
+    return -1;
+  } else {
+    return (int)(device.batteryLevel * 100);
+  }
+}
+@end
